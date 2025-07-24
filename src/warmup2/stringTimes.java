a71f1467956a3cbb823be1a5b230b1e8a6f66183
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-24 11:46:31 +0400

    Add solution for warmup2-stringTimes

---
+++ src/warmup2/stringTimes.java
@@ -0,0 +1,11 @@
+package warmup2;
+
+public class stringTimes {
+    public String stringTimes(String str, int n) {
+        String ni = "";
+        for (int i=0; i<n; i++) {
+            ni += str;
+        }
+        return ni;
+    }
+}
