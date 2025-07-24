198088c5fe11c712ed8be37881612f959745487f
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-24 11:08:46 +0400

    Add solution for warmup1-mixStart

---
+++ src/warmup1/mixStart.java
@@ -0,0 +1,12 @@
+package warmup1;
+
+public class mixStart {public boolean mixStart(String str) {
+    if (str.length() < 3) return false;
+    String two = str.substring(1, 3);
+    if (two.equals("ix")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+}
