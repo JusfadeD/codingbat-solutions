1d03743034c7d2b1d3306a3d4ef87feabfb44553
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-25 18:32:06 +0400

    Add solution for string1-twoChar

---
+++ src/string1/twoChar.java
@@ -0,0 +1,13 @@
+package string1;
+
+public class twoChar {
+    public String twoChar(String str, int index) {
+        if (str.length() <= 2) {
+            return str.substring(0, 2);
+        }
+        if (index < 0 || index + 2 > str.length()) {
+            return str.substring(0, 2);
+        }
+        return str.substring(index, index + 2);
+    }
+}
