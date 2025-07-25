2c888b2a0df76d8d4bf9d3666fa74e4533a583c2
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-25 19:39:26 +0400

    Add solution for string1-minCat

--- src/string1/frontAgain.java
+++ src/string1/frontAgain.java
@@ -1,4 +1,12 @@
 package string1;
 
 public class frontAgain {
+    public boolean frontAgain(String str) {
+        if (str.length() < 2) {
+            return false;
+        }
+        String firstTwo = str.substring(0, 2);
+        String lastTwo = str.substring(str.length() - 2);
+        return firstTwo.equals(lastTwo);
+    }
 }
