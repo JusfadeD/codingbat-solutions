eb11d984241b59477366c497dc055ad196638605
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-26 22:04:22 +0400

    Add solution for logic1-sortaSum

---
+++ src/logic1/sortaSum.java
@@ -0,0 +1,13 @@
+package logic1;
+
+public class sortaSum {
+    public int sortaSum(int a, int b) {
+        int sum = a + b;
+
+        if (sum >= 10 && sum <= 19) {
+            return 20;
+        } else {
+            return sum;
+        }
+    }
+}
