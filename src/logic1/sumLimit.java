b753bae0df12c5d69b805548474bd29b2500132f
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-26 22:23:04 +0400

    Add solution for logic1-sumLimit

---
+++ src/logic1/sumLimit.java
@@ -0,0 +1,16 @@
+package logic1;
+
+public class sumLimit {
+    public int sumLimit(int a, int b) {
+        int sum = a + b;
+
+        String aStr = String.valueOf(a);
+        String sumStr = String.valueOf(sum);
+
+        if (sumStr.length() == aStr.length()) {
+            return sum;
+        } else {
+            return a;
+        }
+    }
+}
