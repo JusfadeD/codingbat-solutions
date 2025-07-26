59fe29ca0a82e5a8cb3eb0ed85bb0eb0c646b611
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-26 21:38:11 +0400

    Add solution for array1-frontPiece

---
+++ src/array1/frontPiece.java
@@ -0,0 +1,12 @@
+package array1;
+
+public class frontPiece {
+    public int[] frontPiece(int[] nums) {
+        if(nums.length<2){
+            return nums;
+        }
+        else{
+            return new int[]{nums[0],nums[1]};
+        }
+    }
+}
