29c55bd432b1ce586e9ee96fe3c2cddd73dfde16
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-27 11:38:29 +0400

    Add solution for string3-sumNumbers

---
+++ src/sumNumbers.java
@@ -0,0 +1,25 @@
+public class sumNumbers {
+    public int sumNumbers(String str) {
+        int sum = 0;
+        String num = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                num = num + ch;
+            } else {
+                if (!num.equals("")) {
+                    sum = sum + Integer.parseInt(num);
+                    num = "";
+                }
+            }
+        }
+
+        if (!num.equals("")) {
+            sum = sum + Integer.parseInt(num);
+        }
+
+        return sum;
+    }
+}
