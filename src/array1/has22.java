8fd232ab5e76e32dbb24ca7439c8707c80a3a0ac
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-28 12:21:19 +0400

    Add solution for Array2-has22

---
+++ src/array1/has22.java
@@ -0,0 +1,12 @@
+package array1;
+
+public class has22 {
+    public boolean has22(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 2 && nums[i + 1] == 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
