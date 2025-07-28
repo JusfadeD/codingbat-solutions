de9a33becd62960aae7403119a1d04bd0e5ac011
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-28 13:24:13 +0400

    Add solution for ap1-dividesSelf

---
+++ src/ap1/dividesSelf.java
@@ -0,0 +1,17 @@
+package ap1;
+
+public class dividesSelf {
+    public boolean dividesSelf(int n) {
+        int original = n;
+
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit == 0 || original % digit != 0) {
+                return false;
+            }
+            n /= 10;
+        }
+
+        return true;
+    }
+}
