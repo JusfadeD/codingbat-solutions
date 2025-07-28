61523af7746617b2c3a86d67243aae3ea8cabba0
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-28 12:42:12 +0400

    Add solution for array1-isEverywhere

---
+++ src/Array2/isEverywhere.java
@@ -0,0 +1,12 @@
+package Array2;
+
+public class isEverywhere {
+    public boolean isEverywhere(int[] nums, int val) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != val && nums[i + 1] != val) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
