a54628a97facacc89f94f6a48f51f92a6148054c
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-28 13:29:02 +0400

    Add solution for ap1-sumHeights

---
+++ src/ap1/sumHeights.java
@@ -0,0 +1,13 @@
+package ap1;
+
+public class sumHeights {
+    public int sumHeights(int[] heights, int start, int end) {
+        int sum = 0;
+
+        for (int i = start; i < end; i++) {
+            sum += Math.abs(heights[i + 1] - heights[i]);
+        }
+
+        return sum;
+    }
+}
