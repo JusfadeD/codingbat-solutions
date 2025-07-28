d3d1d2b33342ea70c3533341f0a820b342535540
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-28 12:53:45 +0400

    Add solution for array1-evenOdd

---
+++ src/Array2/evenOdd.java
@@ -0,0 +1,19 @@
+package Array2;
+
+public class evenOdd {
+    public int[] evenOdd(int[] nums) {
+        int[] result = new int[nums.length];
+        int evenIndex = 0;
+        int oddIndex = nums.length - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                result[evenIndex++] = nums[i];
+            } else {
+                result[oddIndex--] = nums[i];
+            }
+        }
+        
+        return result;
+    }
+}
