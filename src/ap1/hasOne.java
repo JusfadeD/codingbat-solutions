b0df3206b0ae0bf88f70fb1e95ff57b8988b0b9f
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-28 13:23:35 +0400

    Add solution for ap1-hasOne

---
+++ src/ap1/hasOne.java
@@ -0,0 +1,13 @@
+package ap1;
+
+public class hasOne {
+    public boolean hasOne(int n) {
+        while (n > 0) {
+            if (n % 10 == 1) {
+                return true;
+            }
+            n /= 10;
+        }
+        return false;
+    }
+}
