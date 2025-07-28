622030d4a096bbf6713ebfe0328daaf271d24a50
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-28 13:21:13 +0400

    Add solution for ap1-wordsCount

---
+++ src/ap1/wordsCount.java
@@ -0,0 +1,15 @@
+package ap1;
+
+public class wordsCount {
+    public int wordsCount(String[] words, int len) {
+        int count = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() == len) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
