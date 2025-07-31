9e7c807150866794a804e346e061b50ea59d83af
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-31 20:13:00 +0400

    Add solution for map1-topping1

---
+++ src/Map1/topping1.java
@@ -0,0 +1,9 @@
+package Map1;
+
+public class topping1 {
+    git checkout -b map1-topping1
+    git add .
+    git commit -m "Add solution for map1-topping1"
+    git push -u origin map1-topping1
+
+}
