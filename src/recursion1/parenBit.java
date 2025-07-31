11750bf534217a7c595b99ddd7949be7e913d495
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-31 19:58:35 +0400

    Add solution for recursion1-nestParen

--- src/recursion1/parenBit.java
+++ src/recursion1/parenBit.java
@@ -1,4 +1,15 @@
 package recursion1;
 
 public class parenBit {
+    public String parenBit(String str) {
+        if (str.charAt(0) != '(') {
+            return parenBit(str.substring(1));
+        }
+
+        if (str.charAt(str.length() - 1) != ')') {
+            return parenBit(str.substring(0, str.length() - 1));
+        }
+
+        return str;
+    }
 }
