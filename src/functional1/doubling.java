0cfa0ee2146a44dda23137f77b0f2b59f2fceb1c
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-08-02 19:44:46 +0400

    Add solution for functional1-doubling

---
+++ src/functional1/doubling.java
@@ -0,0 +1,9 @@
+package functional1;
+
+public class doubling {
+    public List<Integer> doubling(List<Integer> nums) {
+        return nums.stream()
+                .map(n -> n * 2)
+                .collect(Collectors.toList());
+    }
+}
