c2224f2a95701a541bb3bdc6671e1cdd5a499dec
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-08-02 19:50:50 +0400

    Add solution for functional2-no9

---
+++ src/functional2/no9.java
@@ -0,0 +1,9 @@
+package functional2;
+
+public class no9 {
+    public List<Integer> no9(List<Integer> nums) {
+        return nums.stream()
+                .filter(n -> n % 10 != 9)
+                .collect(Collectors.toList());
+    }
+}
