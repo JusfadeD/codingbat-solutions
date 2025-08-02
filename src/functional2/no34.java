068b777936bba1820258979182c91dbaa903dd8d
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-08-02 19:53:00 +0400

    Add solution for functional2-no34

---
+++ src/functional2/no34.java
@@ -0,0 +1,9 @@
+package functional2;
+
+public class no34 {
+    public List<String> no34(List<String> strings) {
+        return strings.stream()
+                .filter(s -> s.length() != 3 && s.length() != 4)
+                .collect(Collectors.toList());
+    }
+}
