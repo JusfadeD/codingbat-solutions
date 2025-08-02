d6248062e2330b6e6c58956a7f9c0a1cc49fac54
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-08-02 19:47:02 +0400

    Add solution for functional1-moreY

---
+++ src/functional1/moreY.java
@@ -0,0 +1,9 @@
+package functional1;
+
+public class moreY {
+    public List<String> moreY(List<String> strings) {
+        return strings.stream()
+                .map(s -> "y" + s + "y")
+                .collect(Collectors.toList());
+    }
+}
